a545f9c56a37a2647e6a9c803e17f10bc72dd788
Franco Leiva <franco.1995.l@hotmail.com>
2022-12-11 19:03:06 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    //Value nos permite inyectar una propiedad del application properties en una aplicacion java
+    @Value("${app.message}")
+    String message;
+
+    @GetMapping("/hola")
+    public String Hola(){
+        System.out.println(message);
+        return "Hola mundo que tal vamos, SOY MICHIGAN";
+    }
+
+    @GetMapping("/bootstrap")
+    public String bootstrap(){
+        return """
+                
+                <!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Bootstrap demo</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h1>Hola, desde Spring!</h1>
+                    <a class="btn btn-primary" href="http://google.com/"> Google </a>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+                  </body>
+                </html>
+                
+                
+                """;
+    }
+
+}
